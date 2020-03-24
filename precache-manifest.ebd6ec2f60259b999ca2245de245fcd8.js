e0bf128462d829a48791aacb8ecd5dd0c3eef02d
Hayk An <say@hihayk.com>
2020-03-24 21:39:24 +0100

    Updates

---
+++ precache-manifest.ebd6ec2f60259b999ca2245de245fcd8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11c0947f9fa347c3063496f9d5d81853",
+    "url": "/matter/index.html"
+  },
+  {
+    "revision": "214af7e7e1c9a308263a",
+    "url": "/matter/static/css/main.2f4cbe4e.chunk.css"
+  },
+  {
+    "revision": "7472e3b4762db44119fa",
+    "url": "/matter/static/js/2.deff2a09.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/matter/static/js/2.deff2a09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "214af7e7e1c9a308263a",
+    "url": "/matter/static/js/main.aea37f5f.chunk.js"
+  },
+  {
+    "revision": "2effe8f42606ce4deaf4",
+    "url": "/matter/static/js/runtime-main.f02ff4b6.js"
+  },
+  {
+    "revision": "277dd983fb274fc9ccff0b4dde81a5f2",
+    "url": "/matter/static/media/AtlasTypewriterRegular.277dd983.woff"
+  },
+  {
+    "revision": "ed185206e540bce16b62716ce33d38b2",
+    "url": "/matter/static/media/AtlasTypewriterRegular.ed185206.woff2"
+  },
+  {
+    "revision": "759e3341c4cbe36a92d50606ae514ea5",
+    "url": "/matter/static/media/MarrSansWeb-Regular.759e3341.woff"
+  },
+  {
+    "revision": "a94220041d6311d364f0c5edb005b13d",
+    "url": "/matter/static/media/MarrSansWeb-Regular.a9422004.woff2"
+  }
+]);
